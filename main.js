8e7888f2cb1de20eaa1fa9e3657dd82890e5f27c
Squall.Sung 宋翊誠 <Squall.Sung@tp.wanin.tw>
2021-02-20 11:07:54 +0800

    upload test file

---
+++ main.js
@@ -0,0 +1,12 @@
+$("h1").css({"color" : "red"});
+
+$("#list").html('GGDDDSSS');
+
+$(".item").css({"color" : "yellow"});
+
+
+$(document).ready(function(){
+    $('.BBBB').bxSlider();
+  });
+
+//document.getElementsByTagName("h1")[0].style.color = "blue";
